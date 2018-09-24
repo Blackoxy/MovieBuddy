1ee65112ecdc65ea06fdc4e69ff27ded98f2ac65
Brice Sinkule <bsinkule@gmail.com>
2018-09-24 15:02:33 -0600

    added logo to secret

--- src/components/Secret.js
+++ src/components/Secret.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react'
+import logo from "../assets/movie-buddy-logo.png"
 
 class Secret extends Component {
     render(){
         return (
             <div>
+                <img className="buddyLogo" src={logo} alt="movie buddy logo"/>
                 Secret Area<br/>
                 <a href="/">Home</a>
                 <br/>
