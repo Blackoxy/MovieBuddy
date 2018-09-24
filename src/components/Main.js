8dcb3548100c0bd3c0e5092a785226a70a3279cf
Brice Sinkule <bsinkule@gmail.com>
2018-09-24 14:25:10 -0600

    added logo

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import "../App.css"
+import logo from "../assets/movie-buddy-logo.png"
 
 class Main extends Component {
     render(){
         return (
             <div>
+                <img className="buddyLogo" src={logo} alt="movie buddy logo"/>
                 <p className="text-danger">
                 To see your wishlist and buddy matches <a href="/secret">Click Here</a>
                 </p>
