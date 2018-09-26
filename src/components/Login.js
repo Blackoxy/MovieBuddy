bf7e0f5e1152e6ef0f27d5e97d286e5a3476f475
Jose Stricklin <josestricklin@El-Diablo.local>
2018-09-25 22:58:25 -0600

    update login page button

--- src/components/Login.js
+++ src/components/Login.js
@@ -18,43 +18,42 @@ class Main extends React.Component {
     }
 
 
-render(){
-    return (
-        <section className="container-fluid">
-            <div className="main-content">
-                <div className="row">
-                    <div className="col-sm-8 mx-auto">
-                        <div>
-                            <img className="buddyLogo" src={logo} alt="movie buddy logo"/>
-                            <p className="text-danger">
-                                <a href="/secret">Click here to login</a>
-                            </p>
+    render(){
+        return (
+            <section className="container-fluid">
+                <div className="main-content">
+                    <div className="row">
+                        <div className="col-sm-8 mx-auto">
+                            <div>
+                                <div className="header">
+                                    <img className="buddyLogo" src={logo} alt="movie buddy logo"/>
+                                </div>
+                                <button className="loginButton"><a href="/secret">Login/Signup</a></button>
+                                {!this.props.auth.isAuthenticated() && <button className="btn btn-info" onClick={this.props.auth.login}>Login</button>}
 
-                            {!this.props.auth.isAuthenticated() && <button className="btn btn-info" onClick={this.props.auth.login}>Login</button>}
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row d-flex justify-content-space-between m-0">
+                        <div className="main-nav">
+                            <div className="col-sm-6 ml-auto">
+                                {this.state.visible ? <nav>
+                                    <ul>
+                                        <li><a>About</a></li>
+                                        <li><a>Contact Us</a></li>
+                                    </ul>
+                                </nav> : null}
+                            </div>
 
+
+                            { this.state.visible ? <FontAwesomeIcon onClick={this.toggle} className="fa-2x nav-toggle" icon={faTimes} /> : <FontAwesomeIcon onClick={this.toggle} className="fa-2x nav-toggle" icon={faBars}  /> }
                         </div>
                     </div>
                 </div>
-                <div className="row d-flex justify-content-space-between m-0">
-                    <div className="main-nav">
-                        <div className="col-sm-6 ml-auto">
-                            {this.state.visible ? <nav>
-                                <ul>
-                                    <li><a>About</a></li>
-                                    <li><a>Contact Us</a></li>
-                                </ul>
-                            </nav> : null}
-                        </div>
+            </section>
+            )
 
-
-                        { this.state.visible ? <FontAwesomeIcon onClick={this.toggle} className="fa-2x nav-toggle" icon={faTimes} /> : <FontAwesomeIcon onClick={this.toggle} className="fa-2x nav-toggle" icon={faBars}  /> }
-                    </div>
-                </div>
-            </div>
-        </section>
-        )
-
-    }
+}
 }
 
 export default Main;
