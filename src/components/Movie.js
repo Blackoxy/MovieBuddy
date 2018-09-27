7dd7e673c24d4414aa62766906c9a3646a19e7c2
Jose Stricklin <josestricklin@El-Diablo.local>
2018-09-27 00:51:03 -0600

    update movie list and general styles

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,22 +1,32 @@
 import React from 'react'
 import AddMovie from './AddMovie'
+import { Modal } from 'react-bootstrap'
 
 const Movie = (props) => {
     const poster = 'https://image.tmdb.org/t/p/original'
 
     return (
 
-        <li className="contain">
+        <li className="contain bg-eggplant">
             <img className="resp" src={`${poster}${props.poster_path}`} alt="Movie"/>
             <div className="body">
                 <h4>{props.title}</h4>
                 {/* <p>{props.overview}</p> */}
             </div>
-            {/* <button className="movieButton btn btn-info">Add to Wishlist</button> */}
             <AddMovie title={props.title} />
         </li>
 
     )
+}
+
+class MovieModal extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            visible: this.props.visible
+        }
+    }
+
 }
 
 export default Movie
