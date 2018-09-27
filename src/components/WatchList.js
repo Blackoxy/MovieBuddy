78fa41696c1c4829c26dfc7047401781a63c29c4
Brice Sinkule <bsinkule@gmail.com>
2018-09-27 15:49:34 -0600

    buddy match added

--- src/components/WatchList.js
+++ src/components/WatchList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Button } from 'react-bootstrap'
+import MovieBuddyMatch from './MovieBuddyMatch'
 
 const WatchList = (props) => {
     let go = (match) => {
@@ -15,12 +16,13 @@ const WatchList = (props) => {
     let movieArr = go(props.movieData)
 
     const movieMap = movieArr.map(movie => {
-        return <WatchItem title={movie.title} id={movie.id} deleteOne={props.deleteOne}/>
+        return <WatchItem  title={movie.title} id={movie.id} deleteOne={props.deleteOne}/>
         })
 
     return (
         <div className="form bg-caramel mx-auto py-3">
-            <h1>Watch List</h1>
+                <MovieBuddyMatch movieData={props.movieData}/>
+            <h4>Watch List</h4>
             <div>
                 {movieMap}
             </div>
