44f8dc34484f742be3b47f0f95478a5ccdae0e71
Jose Stricklin <josestricklin@El-Diablo.local>
2018-09-28 21:14:42 -0600

    deploy w working modal

--- src/components/WatchList.js
+++ src/components/WatchList.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button } from 'react-bootstrap'
+import { Button } from 'reactstrap'
 import MovieBuddyMatch from './MovieBuddyMatch'
 
 const WatchList = (props) => {
